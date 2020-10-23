a1f1c24e87f2a5143bdd1645cd56e4db3a485d20
dsdiois <dsdiois98@gmail.com>
2020-10-23 13:30:46 +0200

    comentario

--- group10/operator/replacement.py
+++ group10/operator/replacement.py
@@ -2,7 +2,7 @@ from group10.operator import ReplacementOperator
 from group10.population import Population
 import numpy as np
 
-
+#steady-state
 class SteadyStateReplacement(ReplacementOperator):
     def apply(self, population_a, population_b):
         pass
