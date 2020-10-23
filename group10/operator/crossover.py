72d65eba8471001a6b1c4e01dc827464c1f2ec1f
Xiao <xiao.ye@alumnos.upm.es>
2020-10-23 17:21:17 +0200

    Changed Population and Genome structure

--- group10/operator/crossover.py
+++ group10/operator/crossover.py
@@ -10,9 +10,8 @@ class UniformCrossover(CrossoverOperator):
         genome_a, genome_b = genome_pairs[0], genome_pairs[1]
         genome_a_copy = copy(genome_a)
         genome_b_copy = copy(genome_b)
-        equal = genome_a == genome_b
         for i in range(len(genome_a)):
-            if not equal[i]:
+            if genome_a_copy[i] != genome_b[i]:
                 change = random()
                 if change > prob:
                     genome_a_copy[i], genome_b_copy[i] = genome_b_copy[i], genome_a_copy[i]
