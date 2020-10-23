2d6fee96ef2ac33a4c2d0a2da23d39b5b7684965
juan-vmarin <62159985+juan-vmarin@users.noreply.github.com>
2020-10-23 18:03:16 +0200

    Selection done

--- group10/operator/selection.py
+++ group10/operator/selection.py
@@ -1,9 +1,24 @@
 from group10.operator import SelectionOperator
-from group10.population import Population
-from group10.component import Genome
-import numpy as np
+import random
 
 
 class TournamentSelection(SelectionOperator):
-    def apply(self, population):
-        pass
+    def apply(self, population, k=10):
+        # Creation of the lists
+        best = random.choice(population)
+        selected = {best}
+
+        # Check k times
+        for i in range(k - 1):
+            # First case
+            rand = random.choice(population)
+            while rand in selected:
+                rand = random.choice(population)
+
+            if best.fitness < rand.fitness:
+                selected.add(best)
+                best = rand
+
+            selected.add(rand)
+
+        return best
