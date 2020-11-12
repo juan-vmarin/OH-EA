e24fa493a506300190e94096692cda6d9b00b137
juan-vmarin <62159985+juan-vmarin@users.noreply.github.com>
2020-11-12 19:24:39 +0100

    comentadas las clases replacement y selection

--- group10/operator/replacement.py
+++ group10/operator/replacement.py
@@ -3,5 +3,11 @@ from group10.operator import ReplacementOperator
 
 class SteadyStateReplacement(ReplacementOperator):
     def apply(self, population_current, population_offspring):
+        """it represents an operation to generate a new population of genomes
+
+        Args:
+            population_current (component.Population): the current population of genomes
+            population_offspring (component.Population): the population from the current population will change
+        """
         population_current[population_current.worst_index] = population_offspring[0]
         # return population_current
