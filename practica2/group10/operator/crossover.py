0a4249103d6eb0e8af93a4ddd2f2056b85eb8944
juan-vmarin <62159985+juan-vmarin@users.noreply.github.com>
2020-12-26 20:57:29 +0100

    replacement.py and crossover.py done without testing

--- practica2/group10/operator/crossover.py
+++ practica2/group10/operator/crossover.py
@@ -1,4 +1,40 @@
-from practica2.group10.operator import CrossoverOperator
-from random import random
-from copy import copy
-#Binomial
+from group10.operator import CrossoverOperator
+import random
+from random import randint
+import numpy
+
+
+class BinomialCrossover(CrossoverOperator):
+    '''A binomial crossover operator
+    '''
+
+    def apply(self, genomes, cr=0.1):
+        """It represents an operation to cross 2 genomes in a binomial way
+
+        Args:
+            genomes (component.Genome): A list of two genomes
+            cr:
+
+        Returns:
+            Genome: Genome as a result of crossover
+        """
+        v = genomes[0]
+        x = genomes[1]
+        genome_aux = numpy.zeros(len(x))
+
+        for i in range(len(x)):
+            rand = random.random()
+            rand_int = randint(1, 2)
+            if rand <= cr or i == rand_int:
+                genome_aux[i] = v[i]
+            elif rand > cr and i != rand_int:
+                genome_aux[i] = x[i]
+
+        return genome_aux
+
+
+class Test:
+    if __name__ == "__main__":
+        genomes = [4, 7]
+        cr = 0.1
+        pass
