4d4977e79880fb8b95064109a03e4e8125efef8f
dsdiois <dsdiois98@gmail.com>
2021-01-12 23:50:09 +0100

    a little modificacion on crossover.py complete memory

--- practica2/group10/operator/crossover.py
+++ practica2/group10/operator/crossover.py
@@ -1,32 +1,37 @@
 from group10.operator import CrossoverOperator
 import random
 from random import randint
+from copy import copy
 
 
 class BinomialCrossover(CrossoverOperator):
     """A binomial crossover operator
     """
 
-    def apply(self, genomes, cr=0.5):
+    def __init__(self):
+        """
+            cr:possibility to crossover to generate
+        """
+        self.cr = 0.5
+
+    def apply(self, genomes):
         """It represents an operation to cross 2 genomes in a binomial way
 
         Args:
             genomes (component.Genome): A list of two genomes, genomes[0]: donor vector, genomes[1]:target vector
-            cr:possibility to crossover to generate
+
 
         Returns:
             Genome: Genome as a result of crossover
         """
-        v = genomes[0]
-        x = genomes[1]
+        v = copy(genomes[0])
+        x = copy(genomes[1])
         for i in range(len(x)):
             rand = random.random()
             rand_int = randint(1, 2)
-            if rand <= cr or i == rand_int:
+            if rand <= self.cr or i == rand_int:
                 pass
             else:
                 v[i] = x[i]
 
         return v
-
-
